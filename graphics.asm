a21192ba4d901d8d77f7f90b00bcae7e934c0135
Adi-vig <aditya2004cs@gmail.com>
2023-10-19 13:08:23 +0530

    removed cmts

--- graphics.asm
+++ graphics.asm
@@ -1,10 +1,3 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;         Distributed under the Boost Software License, Version 1.0.          ;;
-;;            (See accompanying file LICENSE or copy at                        ;;
-;;                 https://www.boost.org/LICENSE_1_0.txt)                      ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
 global create_window
 global try_get_event
 
